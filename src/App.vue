46fe44c86dd76ccb716c12b37a00b1f0360f328d
ManuelCastro <jesus.castro.pech@gmail.com>
2024-04-18 15:10:23 -0500

    eliminar comentarios

--- src/App.vue
+++ src/App.vue
@@ -36,34 +36,34 @@ nav {
 }
 
 nav a.router-link-exact-active {
-  color: #fff; /* Cambiar color del texto de los enlaces activos */
-  background-color: #555; /* Cambiar color de fondo de los enlaces activos */
-  padding: 0.5rem 1rem; /* Aumentar el espacio alrededor de los enlaces activos */
-  border-radius: 0.25rem; /* Agregar bordes redondeados a los enlaces activos */
+  color: #fff; 
+  background-color: #555;
+  padding: 0.5rem 1rem; 
+  border-radius: 0.25rem; 
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: #666; /* Cambiar color de fondo al pasar el cursor sobre los enlaces activos */
+  background-color: #666; 
 }
 
 nav a {
-  color: #ccc; /* Cambiar color del texto de los enlaces */
-  text-decoration: none; /* Eliminar subrayado de los enlaces */
-  padding: 0.5rem 1rem; /* Aumentar el espacio alrededor de los enlaces */
-  border-left: 1px solid #555; /* Cambiar color del borde izquierdo de los enlaces */
-}
+  color: #ccc; 
+  text-decoration: none; 
+  padding: 0.5rem 1rem; 
+  border-left: 1px solid #555; 
 
 nav a:first-of-type {
-  border-left: none; /* Eliminar borde izquierdo del primer enlace */
+  border-left: none; 
 }
 
 @media (min-width: 1024px) {
   nav {
     text-align: left;
-    margin-left: 0; /* Eliminar margen izquierdo */
+    margin-left: 0; 
     font-size: 1rem;
     padding: 1rem 0;
-    margin-top: 0.5rem; /* Reducir el margen superior */
+    margin-top: 0.5rem; 
   }
+}
 }
 </style>
